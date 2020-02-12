6146bd7c40990304b9db4adba250a1965c1c66d8
JoseRamirez25 <espontaneo25@gmail.com>
2020-02-12 16:59:48 -0600

    Busqueda de datos

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
-document.addEventListener("DOMContentLoaded", e=>{
-    document.querySelector("#frmAlumnos").addEventListener("submit", event=>{
-        event.preventDefault();
+document.addEventListener("DOMContentLoaded", (event) => {
+    const formAlumnos = document.querySelector("#frmAlumno");
 
-        var ClaveCodigo = "Codigo"+Codigo;
-        var ClaveNombre = "Nombre"+Codigo;
-        var ClaveDireccion = "Direccion"+Codigo;
-        var ClaveTelefono = "Telefono"+Codigo;
+    formAlumnos.addEventListener("submit",(e) => {
+        e.preventDefault();
+
+        let Codigo = document.querySelector("#txtCodigoAlumno").value,
+            Nombre = document.querySelector("#txtNombreAlumno").value,
+            Direccion = document.querySelector("#txtDireccionAlumno").value,
+            Telefono = document.querySelector("#txtTelefonoAlumno").value;
             
+        var KeyCodigo = "Codigo"+Codigo;
+        var KeyNombre = "Nombre"+Codigo;
+        var KeyDireccion = "Direccion"+Codigo;
+        var KeyTelefono = "Telefono"+Codigo;
 
-        let codigo = document.querySelector("#txtCodigoAlumno").value,
-            nombre = document.querySelector("#txtNombreAlumno").value,
-            direccion = document.querySelector("#txtDireccionAlumno").value,
-            telefono = document.querySelector("#txtTelefonoAlumno").value;
-
-
-        console.log(codigo, nombre, direccion, telefono);
-        
-        if( 'localStorage' in window ){
-            window.localStorage.setItem("codigo", codigo);
-            window.localStorage.setItem("nombre", nombre);
-            window.localStorage.setItem("direccion", direccion);
-            window.localStorage.setItem("telefono", telefono);
-        } else {
-            alert("Por favor ACTUALIZATE!!!.");
+        if('localStorage' in window){
+            window.localStorage.setItem(KeyCodigo, Codigo);
+            window.localStorage.setItem(KeyNombre, Nombre);
+            window.localStorage.setItem(KeyDireccion, Direccion);
+            window.localStorage.setItem(KeyTelefono, Telefono);
+        }
+        else{
+            alert("No se pudo Guardar");
         }
     });
-    document.querySelector("#btnRecuperarAlumno").addEventListener("click", event=>{
-        document.querySelector("#txtCodigoAlumno").value = window.localStorage.getItem("codigo");
-        document.querySelector("#txtNombreAlumno").value = window.localStorage.getItem("nombre");
-        document.querySelector("#txtDireccionAlumno").value = window.localStorage.getItem("direccion");
-        document.querySelector("#txtTelefonoAlumno").value = window.localStorage.getItem("telefono");
-    });
+    document.querySelector("#btnRecuperarAlumno").addEventListener("click", (e) => {
+        if('localStorage' in window){
+            let Codigo = document.querySelector("#txtCodigoAlumno").value;
+            if(Codigo != ""){
+                document.querySelector("#txtCodigoAlumno").value = window.localStorage.getItem("Codigo" + Codigo);
+                document.querySelector("#txtNombreAlumno").value = window.localStorage.getItem("Nombre" + Codigo);
+                document.querySelector("#txtDireccionAlumno").value = window.localStorage.getItem("Direccion" + Codigo);
+                document.querySelector("#txtTelefonoAlumno").value = window.localStorage.getItem("Telefono" + Codigo);
+            }
+            else{
+                alert("Agregue el codigo de los datos a recuperar");
+            }
+        }
+        else{
+            alert("No se encuentran los datos solicitados");
+        }
+    })
 });
