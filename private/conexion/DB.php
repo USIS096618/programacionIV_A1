39bf734ad2b49529effad61f67c484ce507f9fd4
USIS096618 <espontaneo25@gmail.com>
2020-03-19 16:48:46 -0600

    corregido

--- private/conexion/DB.php
+++ private/conexion/DB.php
@@ -1,23 +1,20 @@
 <?php
-
-/**
- * Clase de conexion al servidor de BD
- */
-class Conexion{
-    private $conexion='', $result='';
-
-    public function Conexion($server, $user, $pass, $db){
-        $this->conexion = mysqli_connect($server,$user,$pass,$db) or die('NO pude conectarme al servidor de BD');
+    class DB{
+        private $conexion, $result;
+        public function DB($server,$user, $pass,$db){
+            $this->conexion = mysqli_connect($server,$user,$pass,$db) or die(mysqli_error('Error'));
+        }
+        public function consultas($sql){
+            $this->result = mysqli_query($this->conexion, $sql) or die(mysqli_error());
+        }
+        public function obtener_data(){
+            return $this->result->fetch_all(MYSQLI_ASSOC);
+        }
+        public function obtener_respuesta(){
+            return $this->result;
+        }
+        public function id(){
+            return $this->result->id();
+        }
     }
-    public function consultas($sql=''){
-        $this->result = mysqli_query($this->conexion,$sql) or die(mysqli_error($this->conexion));
-    }
-    public function obtener_datos(){
-        return $this->result->fetch_all(MYSQLI_ASSOC);
-    }
-    public function respuesta(){
-        return $this->result;
-    }
-}
-
 ?>
