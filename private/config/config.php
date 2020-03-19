3e7ccde4b8e6ba4c8e1d7a235fc17bcb10508843
USIS096618 <espontaneo25@gmail.com>
2020-03-19 16:59:53 -0600

    hecho

--- private/config/config.php
+++ private/config/config.php
@@ -1,6 +1,4 @@
-<?php
-
-include('../../Conexion/DB.php');
-$conexion = new Conexion('localhost','root','','db_app_academica');
-
+<?php 
+    include('../../Conexion/DB.php');
+    $conexion = new DB('localhost','root','','db_academica');
 ?>
